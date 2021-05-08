6f2356c7d6188498a461926d66cfbdc53066df0d
Rafael Lima <ls.rafael@icloud.com>
2021-05-08 19:48:00 -0300

    Stack

---
+++ implementing_stacks/main.py
@@ -0,0 +1,11 @@
+
+class Stack:
+    def __init__(self):
+        self.stak = []
+
+    # Append an element at the end.
+    def push(self, element):
+        self.stak.append(element)
+
+if __name__ == "__main__":
+    pass
--- importing_libraries/main.py
+++ importing_libraries/main.py
@@ -10,16 +10,14 @@ def fat(n):
 
 def pow(base,pow):
     return my_math.pot(base,pow)
-    
 
 # Main function
 if __name__ == "__main__":
     
+    # Import my own libraries
     print_hi("My Name")
-    
     print(f"Fatorial is: {fat(5)}")
-
     print(f"The power is: {my_math.pot(2,10)}")
-
     print(f"My area of square is: {arsq(100)}")
+
     
