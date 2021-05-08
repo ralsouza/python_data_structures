c9b6b6c524ca85c49190ba234ba198002cc50377
Rafael Lima <ls.rafael@icloud.com>
2021-05-08 13:00:13 -0300

    How to create modules in Python.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # Importing libraries
-import os
-import sys
+import my_math
+from my_math import area_square as arsq
 
 def print_hi(name):
     print(f"Hi, {name}")
 
+def fat(n):
+    return my_math.fat(5)
+
+def pow(base,pow):
+    return my_math.pot(base,pow)
+    
 
 # Main function
 if __name__ == "__main__":
+    
     print_hi("My Name")
+    
+    print(f"Fatorial is: {fat(5)}")
+
+    print(f"The power is: {my_math.pot(2,10)}")
+
+    print(f"My area of square is: {arsq(100)}")
+    
