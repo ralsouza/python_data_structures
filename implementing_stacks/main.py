820c2432559d7e0f1946a6817b5e2deeb1010ba1
Rafael Lima <ls.rafael@icloud.com>
2021-05-09 19:06:30 -0300

    stacks

--- implementing_stacks/main.py
+++ implementing_stacks/main.py
@@ -1,11 +1,46 @@
 
 class Stack:
     def __init__(self):
-        self.stak = []
+        self.stack = []
 
-    # Append an element at the end.
+    # Appending an element at the end.
     def push(self, element):
-        self.stak.append(element)
+        self.stack.append(element)
+        print(f"Element {element} is added.")
+        return None
+
+    # Removing the last element
+    def pop(self):
+        if not self.empty():
+            self.stack.pop(len(self.stack) -1)
+            print("The last element is removed.")
+            return None
+        print("The list is empty.")
+        return None
+
+    # Selecting the top element
+    def top(self):
+        print(f"The top element is {self.stack[-1]}")
+        return None
+
+    # Checking whether the list are empty
+    def empty(self):
+        if (len(self.stack) == 0):
+            return True
+        return False
 
 if __name__ == "__main__":
-    pass
+    
+    s = Stack()
+
+    print(s.empty())
+
+    s.push(1)
+    s.push(2)
+    s.push(3)
+
+    s.top()
+    print(s.empty())
+
+    s.pop()
+    s.top()
