c5ed492323a070d357a5ede66cca638face93f3f
Rafael Lima <ls.rafael@icloud.com>
2021-05-09 19:20:00 -0300

    Optimizing the stack

--- implementing_stacks/main.py
+++ implementing_stacks/main.py
@@ -2,17 +2,20 @@
 class Stack:
     def __init__(self):
         self.stack = []
+        self.len_stack = 0
 
     # Appending an element at the end.
     def push(self, element):
         self.stack.append(element)
+        self.len_stack += 1
         print(f"Element {element} is added.")
         return None
 
     # Removing the last element
     def pop(self):
         if not self.empty():
-            self.stack.pop(len(self.stack) -1)
+            self.stack.pop(self.len_stack - 1)
+            self.len_stack -= 1
             print("The last element is removed.")
             return None
         print("The list is empty.")
@@ -25,9 +28,14 @@ class Stack:
 
     # Checking whether the list are empty
     def empty(self):
-        if (len(self.stack) == 0):
+        if self.len_stack == 0:
             return True
         return False
+
+    # Checking the list length
+    def length(self):
+        print(f"The length is {self.len_stack}")
+        return None
 
 if __name__ == "__main__":
     
@@ -44,3 +52,5 @@ if __name__ == "__main__":
 
     s.pop()
     s.top()
+
+    s.length()
