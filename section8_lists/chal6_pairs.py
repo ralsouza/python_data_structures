490eba4efa571c1c25391ee87dc6f373123b6527
Rafael Lima <ls.rafael@icloud.com>
2021-06-02 08:23:22 -0300

    chal

---
+++ section8_lists/chal6_pairs.py
@@ -0,0 +1,9 @@
+# Pairs
+# Write a function to find all pairs of an integer array whose sum is equal to a given number.
+# Example: pair_sum([2,4,3,5,6,-2,4,7,8,9], 7)
+# Output: ['2+5', '4+3', '3+4', '-2+9']
+
+my_list = [2,4,3,5,6,-2,4,7,8,9]
+
+def pair_sum(list, num_sum):
+    
