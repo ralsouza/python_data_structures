b4816fcd70515714ab86e32823cceb34725f8acd
Rafael Lima <ls.rafael@icloud.com>
2021-06-11 23:11:46 -0300

    bst

---
+++ section23_binary_search_tree/239_create_binary_search_tree.py
@@ -0,0 +1,10 @@
+# Create a Binary Search Tree
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+    
+new_bst = BinarySearchTreeNode(None)
---
+++ section23_binary_search_tree/240_insert_node_binary_search_tree.py
@@ -0,0 +1,14 @@
+# Insert a node to BST
+
+class BinarySearchTreeNode:
+    
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+    
+
+
+    
+new_bst = BinarySearchTreeNode(None)
