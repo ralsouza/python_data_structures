2427da0790ef246cdfc5647b8ccc35c7e3af9889
Rafael Lima <ls.rafael@icloud.com>
2021-06-13 16:39:24 -0300

    BST

--- section23_binary_search_tree/240_insert_node_binary_search_tree.py
+++ section23_binary_search_tree/240_insert_node_binary_search_tree.py
@@ -1,14 +1,32 @@
 # Insert a node to BST
 
-class BinarySearchTreeNode:
-    
+class BSTNode:
     def __init__(self, data):
         self.data = data
         self.left_child = None
         self.right_child = None
 
-    
+def insert_node(root_node, node_value):
+    if root_node.data is None:
+        root_node.data = node_value
+    elif node_value <= root_node.data:
+        if root_node.left_child is None:
+            root_node.left_child = BSTNode(node_value)
+        else:
+            insert_node(root_node.left_child, node_value)
+    else:
+        if root_node.right_child is None:
+            root_node.right_child = BSTNode(node_value)
+        else:
+            insert_node(root_node.right_child, node_value)
+    return "The node has been successfully inserted."
 
+new_bst = BSTNode(None)
 
-    
-new_bst = BinarySearchTreeNode(None)
+print(insert_node(new_bst, 70))
+print(insert_node(new_bst, 60))
+print(insert_node(new_bst, 80))
+
+print(new_bst.data)
+print(new_bst.left_child.data)
+print(new_bst.right_child.data)
