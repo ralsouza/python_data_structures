71f5874c0b9f9483a0aba3df04259c087f9d4dd7
Rafael Lima <ls.rafael@icloud.com>
2021-06-16 00:23:03 -0300

    linear search

---
+++ section29_searching_algoritms/305_linear_search.py
@@ -0,0 +1,12 @@
+# Linear Search
+# 1. Create a function with two parameters which are an array and a value
+# 2. Loop through the array and check if the current array element is equal to the value
+# 3. If it is return the index at which the element is found
+# 4. If the value is never found return -1
+
+arr = [20,40,30,50,90]
+value = 50
+
+def linear_search(array, value):
+    pass
+
