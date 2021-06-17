c69116c93c400c1d7644e8a9322b50a85029cc51
Rafael Lima <ls.rafael@icloud.com>
2021-06-17 18:54:26 -0300

    linear search

--- section29_searching_algoritms/305_linear_search.py
+++ section29_searching_algoritms/305_linear_search.py
@@ -4,9 +4,14 @@
 # 3. If it is return the index at which the element is found
 # 4. If the value is never found return -1
 
-arr = [20,40,30,50,90]
-value = 50
-
 def linear_search(array, value):
-    pass
+    for i in range(len(array)):
+        if array[i] == value:
+            return i
+    return -1
 
+
+arr = [20,40,30,50,90]
+value = 100
+
+print(linear_search(arr, value))
---
+++ section29_searching_algoritms/307_binary_search.py
@@ -0,0 +1,6 @@
+# Binary Search
+# - Binary Search is faster than Linear Search;
+# - Half of the remaining element can be eliminated at a time, 
+#   instead of eliminating them on by one;
+# - ** Binary Search only works for sorted arrays **
+
