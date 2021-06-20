1524e9add625377dc07c866cece4a9843e656290
Rafael Lima <ls.rafael@icloud.com>
2021-06-20 17:15:35 -0300

    bubble sort

--- section28_sorting_algorithms/293_bubbles_sort.py
+++ section28_sorting_algorithms/293_bubbles_sort.py
@@ -2,3 +2,14 @@
 # - Bubble sort is also referred as Sinking sort
 # - We repeatedly compare each pair of adjacent items and swap them if
 #   they are in the wrong order
+
+def bubble_sort(custom_list):
+    for i in range(len(custom_list)-1):
+        for j in range(len(custom_list)-i-1):
+            if custom_list[j] > custom_list[j+1]:
+                custom_list[j], custom_list[j+1] = custom_list[j+1], custom_list[j]
+    print(custom_list)
+
+c_list = [2,1,7,6,5,3,4,9,8]
+
+bubble_sort(c_list)
